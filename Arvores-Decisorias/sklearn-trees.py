dc3508f3a51426122e3e2d9d49d723817a0eecfc
Lucas de Brito Mattos <lucasbritomattos@gmail.com>
2025-12-24 10:36:27 -0300

    aula 10 inicio da atividade de sala

--- Arvores-Decisorias/sklearn-trees.py
+++ Arvores-Decisorias/sklearn-trees.py
@@ -3,11 +3,6 @@ import matplotlib.pyplot
 import graphviz
 import io
 
-# Make a decision tree and train it on the iris datasets
-
-# Load the iris dataset and split it into features and target
-# Visualize graphically the dataset
-
 iris = sklearn.datasets.load_iris()
 ax = matplotlib.pyplot.subplot()
 scatter = ax.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target)
@@ -23,7 +18,7 @@ sample_flower = [[5.0, 1.5, 0.5, 3.5]]
 predicted_class = classifier.predict(sample_flower)
 print(f"The predicted class for the flower with features {sample_flower} is: {iris.target_names[predicted_class][0]}")
 
-# Visualize the decision tree
+# mostra a arvore
 dot_data = io.StringIO()
 sklearn.tree.export_graphviz(classifier, out_file=dot_data, 
                      feature_names=iris.feature_names,  
@@ -32,6 +27,6 @@ sklearn.tree.export_graphviz(classifier, out_file=dot_data,
                      special_characters=True)
 dot_data = dot_data.getvalue()
 graph = graphviz.Source(dot_data)
-graph.render("iris_decision_tree")  # Saves the tree as a PDF file
-graph.view()  # Opens the PDF file
+graph.render("iris_decision_tree")
+graph.view()
 

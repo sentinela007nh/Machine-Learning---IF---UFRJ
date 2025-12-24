db546d19b7abf3ab64c0d8e85237042e5ecf54bd
Lucas de Brito Mattos <lucasbritomattos@gmail.com>
2025-12-24 10:51:56 -0300

    aula10 enviar

--- Arvores-Decisorias/sklearn-trees.py
+++ Arvores-Decisorias/sklearn-trees.py
@@ -1,26 +1,27 @@
-import sklearn
-import matplotlib.pyplot
+from sklearn import datasets, model_selection, tree
+
+import matplotlib.pyplot as plt
 import graphviz
 import io
 
-iris = sklearn.datasets.load_iris()
-ax = matplotlib.pyplot.subplot()
+iris = datasets.load_iris()
+ax = plt.subplot()
 scatter = ax.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target)
 ax.set(xlabel=iris.feature_names[0], ylabel=iris.feature_names[1])
 ax.legend(scatter.legend_elements()[0], iris.target_names, title="Classes")
-matplotlib.pyplot.show()
+plt.show()
 
 
-X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
-classifier = sklearn.tree.DecisionTreeClassifier(max_depth=3, random_state=42)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+classifier = tree.DecisionTreeClassifier(max_depth=3, random_state=42)
 classifier = classifier.fit(X_train, y_train)
-sample_flower = [[5.0, 1.5, 0.5, 3.5]]
+sample_flower = [[5.0, 3.5, 1.5, 0.5]]
 predicted_class = classifier.predict(sample_flower)
-print(f"The predicted class for the flower with features {sample_flower} is: {iris.target_names[predicted_class][0]}")
+print(f"The predicted class for the flower with features {sample_flower[0]} is: {iris.target_names[predicted_class][0]}")
 
 # mostra a arvore
 dot_data = io.StringIO()
-sklearn.tree.export_graphviz(classifier, out_file=dot_data, 
+tree.export_graphviz(classifier, out_file=dot_data, 
                      feature_names=iris.feature_names,  
                      class_names=iris.target_names,
                      filled=True, rounded=True,  
@@ -29,4 +30,3 @@ dot_data = dot_data.getvalue()
 graph = graphviz.Source(dot_data)
 graph.render("iris_decision_tree")
 graph.view()
-

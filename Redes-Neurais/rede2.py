c2b296422314784db6f347c03d1cd3886453df52
Lucas de Brito Mattos <lucasbritomattos@gmail.com>
2025-12-24 10:13:03 -0300

    arquivos a mover

--- Redes-Neurais/rede2.py
+++ Redes-Neurais/rede2.py
@@ -4,5 +4,5 @@ model = keras.Sequential (
     [
         keras.layers.Dense(512, activation="relu"),
         keras.layers.Dense(10, activation="softmax")
-    ]
-model.compile(adamm="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    ])
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
